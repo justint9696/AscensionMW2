615bbb87136dc6359de04639b78e0b7d805c9a42
justint9696 <justint9696@hotmail.com>
2022-05-23 19:53:06 -0600

    remove unnecessary structs

--- AscensionMW2/Struct.h
+++ AscensionMW2/Struct.h
@@ -56,7 +56,7 @@ typedef struct vec2_t {
 	float x, y;
 } vec2;
 
-typedef enum clc_ops_e {
+enum clc_ops_e {
 	clc_move = 0x0,
 	clc_moveNoDelta = 0x1,
 	clc_clientCommand = 0x2,
@@ -76,86 +76,25 @@ struct msg_t {
 	int lastEntityRef;
 };
 
-typedef enum svscmd_type {
+enum svscmd_type {
 	SV_CMD_CAN_IGNORE = 0x0,
 	SV_CMD_RELIABLE = 0x1,
 };
 
-typedef enum clientConnected_t {
+enum clientConnected_t {
 	CON_DISCONNECTED = 0x0,
 	CON_CONNECTING = 0x1,
 	CON_CONNECTED = 0x2,
 };
 
-typedef enum sessionState_t {
+enum sessionState_t {
 	SESS_STATE_PLAYING = 0x0,
 	SESS_STATE_DEAD = 0x1,
 	SESS_STATE_SPECTATOR = 0x2,
 	SESS_STATE_INTERMISSION = 0x3,
 };
 
-struct hudelem_color_t {
-	char r, g, b, a;
-}; 
-   
-struct hudelem_s { 
-	 int type; // 0x00-0x04 
-	 float y; // 0x04-0x08 
-	 float x; // 0x08-0x0C 
-	 float z; // 0x0C-0x10 
-	 int targetEntNum; // 0x10-0x14 
-	 float fontScale; // 0x14-0x18 
-	 float fromFontScale; // 0x18-0x1C 
-	 int fontScaleStartTime; // 0x1C-0x20 
-	 int fontScaleTime; // 0x20-0x24 
-	 int label; // 0x24-0x28 
-	 int font; // 0x28-0x2C 
-	 int alignOrg; // 0x2C-0x30 
-	 int alignScreen; // 0x30-0x34 
-	 hudelem_color_t color; // 0x34-0x38 
-	 hudelem_color_t fromColor; // 0x38-0x3C 
-	 int fadeStartTime; // 0x3C-0x40 
-	 int fadeTime; // 0x40-0x44 
-	 int height; // 0x44-0x48 
-	 int width; // 0x48-0x4C 
-	 int materialIndex; // 0x4C-0x50 
-	 int fromHeight; // 0x50-0x54 
-	 int fromWidth; // 0x54-0x58 
-	 int scaleStartTime; // 0x58-0x5C 
-	 int scaleTime; // 0x5C-0x60 
-	 float fromY; // 0x60-0x64 
-	 float fromX; // 0x64-0x68 
-	 int fromAlignOrg; // 0x68-0x6C 
-	 int fromAlignScreen; // 0x6C-0x70 
-	 int moveStartTime; // 0x70-0x74 
-	 int moveTime; // 0x74-0x78 
-	 float value; // 0x78-0x7C 
-	 int time; // 0x7C-0x80 
-	 int duration; // 0x80-0x84 
-	 int text; // 0x84-0x88 
-	 float sort; // 0x88-0x8C 
-	 hudelem_color_t glowColor; // 0x8C-0x90 
-	 int fxBirthTime; // 0x90-0x94 
-	 int fxLetterTime; // 0x94-0x98 
-	 int fxDecayStartTime; // 0x98-0x9C 
-	 int fxDecayDuration; // 0x9C-0xA0 
-	 int soundID; // 0xA0-0xA4 
-	 int flags; // 0xA4-0xA8 
-}; 
-   
-struct game_hudelem_s { 
-	 hudelem_s elem; // 0x00-0xA8 
-	 int clientNum; // 0xA8-0xAC 
-	 int teamNum; // 0xAC-0xB0 
-	 int archived; // 0xB0-0xB4 
-}; 
-
-struct hudData_s {
-	hudelem_s current[31]; // 0x0
-	hudelem_s archival[31]; // 0x1458
-};
-
-typedef enum clientStateStatus_t {
+enum clientStateStatus_t {
 	CS_FREE = 0x0,
 	CS_ZOMBIE = 0x1,
 	CS_RECONNECTING = 0x2,
@@ -198,7 +137,7 @@ struct viewClampState {
 	float startTime; // 0x3C
 };
 
-typedef enum team_t {
+enum team_t {
 	TEAM_FREE = 0x0,
 	TEAM_AXIS = 0x1,
 	TEAM_ALLIES = 0x2,
@@ -302,7 +241,7 @@ struct stats_s {
 	uint32_t winStreak; // 0x54
 };
 
-typedef enum testClientType_t {
+enum testClientType_t {
 	TC_NONE = 0x0,
 	TC_TEST_CLIENT = 0x1,
 	TC_BOT = 0x2,
